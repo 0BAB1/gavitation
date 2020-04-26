2120e77882d2daf5f25bf77d35e0e2cc65190997
0BAB1 <62248962+0BAB1@users.noreply.github.com>
2020-04-26 18:33:36 +0100

    finish

--- entity.py
+++ entity.py
@@ -3,16 +3,12 @@ import math
 import pygame.gfxdraw
 
 class Entity(pygame.sprite.Sprite):
-    def __init__(self, Ox, Oy, Vx, Vy, Ax, Ay, mass):
+    def __init__(self, Ox, Oy, Vx, Vy, Ax, Ay, mass, fixed):
         super().__init__()
         self.mass = mass
         self.image = 0
-        self.draw()
-        self.rect = self.image.get_rect()
-
-        #données affichage
-        self.rect.x = Ox
-        self.rect.y = Oy
+        self.rect = 0
+        self.draw(Ox,Oy)
 
         #données rélles pour calculs car on donne que des donnés en pixels, on converti en m selon l'echelle
         self.x = Ox * 400000
@@ -26,45 +22,55 @@ class Entity(pygame.sprite.Sprite):
         self.Ax = 0
         self.angleC = 0
         self.angleS = 0
+
+        #pour fixer la planete
+
+        self.fixed = fixed
+
+        self.w = 0
     
     def attract(self, EntToAttract): #entité qui influe sur l'accélération
 
         #calculs avec les données réelles
         self.d = math.sqrt( (EntToAttract.x-self.x)**2 + (EntToAttract.y-self.y)**2 )
-        
-        self.A = (EntToAttract.mass * (6.67*(10**(-11))))/ self.d**2
-        
-        self.angleC = math.acos((EntToAttract.x-self.x)/self.d)
-        self.angleS = math.asin((EntToAttract.y-self.y)/self.d)
-        self.Ax = self.A * math.cos(self.angleC)
-        self.Ay = self.A * math.sin(self.angleS)
 
-        #on adapte les vraies données
-
-        self.rect.x = self.x / 400000
-        self.rect.y = (self.y) / 400000
+        if not self.d == 0:
+            
+            self.A = (EntToAttract.mass * (6.67*(10**(-11))))/ self.d**2
+            
+            self.angleC = math.acos((EntToAttract.x-self.x)/self.d)
+            self.angleS = math.asin((EntToAttract.y-self.y)/self.d)
+            self.Ax = self.A * math.cos(self.angleC)
+            self.Ay = self.A * math.sin(self.angleS)
     
     def move(self):
-        self.Vx += self.Ax * 0.4
-        self.Vy += self.Ay * 0.4
 
-        self.x += self.Vx * 0.4
-        self.y += self.Vy * 0.4
+        if not self.fixed:
+            self.Vx += self.Ax * 0.04
+            self.Vy += self.Ay * 0.04
 
-        self.rect.x = self.x / 400000
-        self.rect.y = (self.y) / 400000
+            self.x += self.Vx * 0.04
+            self.y += self.Vy * 0.04
 
-        #on remet les accelérations a 0, elle reprendrons une certaines valeur si on doit en appliquer, sinon elles restent a 0 et la planetes continue sa route
+            self.rect.x = self.x / 400000 - self.w #le - self.w sert a recentrer l'entité apres les calculs
+            self.rect.y = self.y / 400000 - self.w
 
-        self.Ax = 0
-        self.Ay = 0
+            #on remet les accelérations a 0, elle reprendrons une certaines valeur si on doit en appliquer, sinon elles restent a 0 et la planetes continue sa route
 
-    def draw(self):
-        temp_mass = math.log(self.mass / 1000000)
-        w = int(temp_mass/2)
+            self.Ax = 0
+            self.Ay = 0
 
-        ENT_IMG = pygame.Surface((temp_mass, temp_mass), pygame.SRCALPHA)
-        pygame.gfxdraw.aacircle(ENT_IMG, w, w, w, (80, 80, 80))
-        pygame.gfxdraw.filled_circle(ENT_IMG, w, w, w, (80, 80, 80))
+    def draw(self, x, y):
+        temp_mass = math.log(self.mass / 10000000)
+        self.w = int(temp_mass/2)
+
+        ENT_IMG = pygame.Surface((temp_mass, temp_mass ), pygame.SRCALPHA)
+        pygame.gfxdraw.aacircle(ENT_IMG, self.w, self.w, self.w, (80, 80, 80))
+        pygame.gfxdraw.filled_circle(ENT_IMG, self.w, self.w, self.w, (80, 80, 80))
 
         self.image = ENT_IMG
+
+        self.rect = self.image.get_rect()
+
+        self.rect.x = x - self.w  #le - self.w sert a recentrer l'entité apres les calculs
+        self.rect.y = y - self.w
